523ec1d873abb1151bafc3790f7c0523506c8d1b
Brayolay <bhellerrobbins@gmail.com>
2025-12-09 08:25:17 -0800

    stuffs

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
 #include "EZ-Template/api.hpp"
+#include "EZ-Template/piston.hpp"
 #include "api.h"
 
 extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
-inline pros::Motor intake_1(-4);
+inline pros::Motor intake_1(4);
 inline pros::Motor intake_2(-5);
 // inline pros::adi::DigitalIn limit_switch('A');
+
+inline ez::Piston MatchLoad('A');
+inline ez::Piston IntakeRaise('B');
+inline ez::Piston DoublePark('C');
 
 enum class Rtype{
     Full,
